9e64b57196a697e58920b6a86460c4b8bcebff4e
profcpmuniz <121521898+profcpmuniz@users.noreply.github.com>
2023-11-02 18:09:46 +0000

    Workshop live code

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const todoList = [];
 
+function loadTodoList() {
+  let ulElement = document.querySelector(".todoList");
+  let spanElement = document.querySelector(".todoListTotal");
+  ulElement.innerHTML = "";
+  spanElement.innerHTML = "";
+  if (todoList.length === 0) {
+    ulElement.innerHTML = "<li>No Task</li>";
+    return;
+  }
+  spanElement.innerHTML = `${todoList.length} item left`;
+  todoList.forEach(item => {
+    let liElement = document.createElement("li");
+    liElement.innerHTML = item;
+    let buttonElement = document.createElement("button");
+    buttonElement.addEventListener("click", () => {
+      let position = todoList.indexOf(item);
+      todoList.splice(position, 1);
+      loadTodoList();
+    });
+    buttonElement.innerHTML = "Delete";
+    liElement.appendChild(buttonElement);
+    ulElement.appendChild(liElement);
+  });
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let input = document.getElementById("taskInput");
+  input.addEventListener("blur", ev => {
+    let content = ev.target.value;
+    if (content) {
+      todoList.push(content);
+      ev.target.value = "";
+      loadTodoList();
+    }
+  });
 };
